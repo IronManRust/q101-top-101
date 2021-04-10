9970774b3852a8537829f2c82c56ca7a5fd694a7
Shawn Headrick <shawn_headrick@yahoo.com>
2021-04-10 16:06:38 -0500

    CSS / Metadata / Images

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import fs from 'fs'
 import handlebars from 'handlebars'
 import path from 'path'
+import { NormalizedPackageJson } from 'read-pkg'
 import { Countdown } from '../types/countdown'
 
-const data = fs.readFileSync(path.resolve(__dirname, 'countdown.json'), 'utf-8')
-const source = fs.readFileSync(path.resolve(__dirname, 'index.hbs'), 'utf-8')
+const template = handlebars.compile(fs.readFileSync(path.resolve(__dirname, 'index.hbs'), 'utf-8'))
 
-const countdown: Countdown = JSON.parse(data)
-const template = handlebars.compile(source)
-const html = template(countdown)
+const countdown: Countdown = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'countdown.json'), 'utf-8'))
+
+const packageJSON: NormalizedPackageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'))
+const metadata = {
+  author: packageJSON.author?.name,
+  description: packageJSON.description,
+  keywords: packageJSON.keywords
+}
+
+// TODO: YouTube / Wikipedia Links
+// TODO: Web Hosting
+
+const html = template({
+  ...countdown,
+  ...metadata
+})
 
 fs.writeFileSync(path.resolve(__dirname, 'index.html'), html)
